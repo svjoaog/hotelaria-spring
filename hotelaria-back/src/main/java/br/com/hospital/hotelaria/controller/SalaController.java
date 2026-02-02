fd173c4a8bd6f849fce7da9a9a9b28f73f43fed1
João Vitor <s.joaog3@gmail.com>
2026-02-02 11:50:04 -0300

    refactor: divide project into folders

--- hotelaria-back/src/main/java/br/com/hospital/hotelaria/controller/SalaController.java
+++ hotelaria-back/src/main/java/br/com/hospital/hotelaria/controller/SalaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import br.com.hospital.hotelaria.dto.SalaResponse;
 import br.com.hospital.hotelaria.service.SalaService;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/salas")
